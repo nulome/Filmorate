4f7235133373152fdfadc8adb41dca0191290bc0
Luckaaa <perepelitsalesha@yandex.ru>
2024-05-12 13:32:19 +0300

    fix sql strings

--- src/main/java/ru/yandex/practicum/filmorate/storage/event/EventRepositoryImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/EventRepositoryImpl.java
@@ -13,60 +13,42 @@ import java.sql.Date;
 public class EventRepositoryImpl implements EventStorage {
 
     private final JdbcTemplate jdbcTemplate;
-    private static final String ADD_LIKES_EVENT_SQL = "INSERT INTO events " +
+    private static final String EVENT_SQL = "INSERT INTO events " +
                                                       "(user_id, entity_id, event_type, operation, event_date) " +
-                                                      "VALUES (?, ?, 'LIKE', 'ADD', ?)";
-    private static final String REMOVE_LIKES_EVENT_SQL = "INSERT INTO events " +
-                                                         "(user_id, entity_id, event_type, operation, event_date) " +
-                                                         "VALUES (?, ?, 'LIKE', 'REMOVE', ?)";
-    private static final String CREATE_REVIEW_EVENT_SQL = "INSERT INTO events " +
-                                                          "(user_id, entity_id, event_type, operation, event_date) " +
-                                                          "VALUES (?, ?, 'REVIEW', 'ADD', ?)";
-    private static final String UPDATE_REVIEW_EVENT_SQL = "INSERT INTO events " +
-                                                          "(user_id, entity_id, event_type, operation, event_date) " +
-                                                          "VALUES (?, ?, 'REVIEW', 'UPDATE', ?)";
-    private static final String DELETE_REVIEW_EVENT_SQL = "INSERT INTO events " +
-                                                          "(user_id, entity_id, event_type, operation, event_date) " +
-                                                          "VALUES (?, ?, 'REVIEW', 'REMOVE', ?)";
-    private static final String ADD_FRIEND_EVENT_SQL = "INSERT INTO events " +
-                                                       "(user_id, entity_id, event_type, operation, event_date) " +
-                                                       "VALUES (?, ?, 'FRIEND', 'ADD', ?)";
-    private static final String DELETE_FRIEND_EVENT_SQL = "INSERT INTO events " +
-                                                          "(user_id, entity_id, event_type, operation, event_date) " +
-                                                          "VALUES (?, ?, 'FRIEND', 'REMOVE', ?)";
+                                                      "VALUES (?, ?, ?, ?, ?)";
 
     @Override
     public void addLikesHandler(Integer filmId, Integer userId, long timeStamp) {
-        jdbcTemplate.update(ADD_LIKES_EVENT_SQL, userId, filmId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, filmId, "LIKE","ADD", new Date(timeStamp));
     }
 
     @Override
     public void deleteLikesHandler(Integer filmId, Integer userId, long timeStamp) {
-        jdbcTemplate.update(REMOVE_LIKES_EVENT_SQL, userId, filmId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, filmId, "LIKE", "REMOVE", new Date(timeStamp));
     }
 
     @Override
     public void createReviewHandler(Integer reviewId, Integer userId, long timeStamp) {
-        jdbcTemplate.update(CREATE_REVIEW_EVENT_SQL, userId, reviewId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, reviewId, "REVIEW", "ADD", new Date(timeStamp));
     }
 
     @Override
     public void updateReviewHandler(Integer reviewId, Integer userId, long timeStamp) {
-        jdbcTemplate.update(UPDATE_REVIEW_EVENT_SQL, userId, reviewId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, reviewId, "REVIEW", "UPDATE", new Date(timeStamp));
     }
 
     @Override
     public void deleteReviewHandler(Integer userId, Integer reviewId, long timeStamp) {
-        jdbcTemplate.update(DELETE_REVIEW_EVENT_SQL, userId, reviewId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, reviewId, "REVIEW", "REMOVE", new Date(timeStamp));
     }
 
     @Override
     public void addUserFriendHandler(Integer userId, Integer friendId, long timeStamp) {
-        jdbcTemplate.update(ADD_FRIEND_EVENT_SQL, userId, friendId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, friendId, "FRIEND", "ADD", new Date(timeStamp));
     }
 
     @Override
     public void deleteUserFriendHandler(Integer userId, Integer friendId, long timeStamp) {
-        jdbcTemplate.update(DELETE_FRIEND_EVENT_SQL, userId, friendId, new Date(timeStamp));
+        jdbcTemplate.update(EVENT_SQL, userId, friendId, "FRIEND", "REMOVE", new Date(timeStamp));
     }
 }
